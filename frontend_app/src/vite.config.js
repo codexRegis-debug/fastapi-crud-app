0a7b7983fb1a90368e4b6f09a9feb4846f9ef507
codexRegis-debug <quincyedos85@gmail.com>
2026-06-27 15:22:01 +0100

    Update vite.config.js

--- frontend_app/src/vite.config.js
+++ frontend_app/src/vite.config.js
@@ -2,13 +2,8 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: '/fastapi-crud-app/',
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://crud-note-app.onrender.com",
-        changeOrigin: true,
-        rewrite: (path) => path
-      },
-    },
-  } ,
+  build: {
+    outDir: '../docs',
+    emptyOutDir: true
+  }
 });
